e4e707761e8b3cf13424e244cadc41b873cc5c62
Anastasiya <71728264+Anastasizzmay@users.noreply.github.com>
2021-01-28 12:44:36 +0300

    Add files via upload

---
+++ Variables.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+
+    var hello string
+    hello = "Hello Go!"
+    var x int
+    var y float64
+    var a int = 2019
+    fmt.Println(hello)
+    fmt.Println(a)
+}
